9de6e1741b464af2ce44f54bf3f2bb7c70486c14
hydr0g3nz <renbowneos@gmail.com>
2020-01-14 01:29:17 +0700

    test 3

--- hack.py
+++ hack.py
@@ -1,5 +1,29 @@
-def mp2():
-    for i in range(1,13):
-        print(f'2 x {i} = {i*2}')
+import subprocess as s
+import pyautogui as pg 
+import time
+path_chrom = "C:\\Program Files (x86)\\Google\Chrome\\Application\\chrome.exe"
+#s.Popen(path_chrom)
+s.Popen('mspaint.exe')
+time.sleep(2)
+pg.hotkey('win','up')
 
-mp2()
+pg.press('alt')
+pg.press('f')
+pg.press('e')
+time.sleep(1)
+pg.typewrite("1000")
+pg.press('tab')
+pg.typewrite("500")
+pg.press("enter")
+
+import random
+a = random.randint(5,5+1000)
+b = random.randint(212,212+500)
+pg.moveTo(a,b)
+for i in range (100):
+    print('Line : ',i)
+    x = random.randint(5,5+1000)
+    y = random.randint(212,212+500)
+    pg.dragTo(x,y,button='left')
+
+#5,212
