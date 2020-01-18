c33644b8bc0cbcd840b2332d80c6e5332b44a18b
hydr0g3nz <renbowneos@gmail.com>
2020-01-18 13:10:04 +0700

    add hidefolder.py

---
+++ hidefolder.py
@@ -0,0 +1,17 @@
+import os
+
+path = os.getcwd()
+print(f'path is {path}')
+
+allfolder = os.listdir(path)
+allcommand = ""
+for i,f in enumerate(allfolder):
+    #print(f'{i+1}.{f}')
+    if len(f.split(".")) > 1 :
+        print("Don't hide : "+ f)
+    else:
+        print(f'attrib +h "{f}"')
+        allcommand = allcommand + 'attrib +h "{}"\n.format{f}'
+print("-"*20)
+print(allcommand)
+
